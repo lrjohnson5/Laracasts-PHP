0f29aca107cc5dc2233082e35932b8323d6b96cf
Lindsay Johnson <lrjohnson5@yahoo.com>
2024-10-02 10:05:56 -0500

    Lesson 43: Extract an Authenticator Class

---
+++ Core/Authenticator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Core;
+
+class Authenticator
+{
+    public function attempt($email, $password) {
+        $user = App::resolve(Database::class)
+            ->query('SELECT * FROM users WHERE email = :email', [
+            'email' => $email
+        ])->find();
+
+        if ($user) {
+            // have a user, now make sure password matches
+            if (password_verify($password, $user['password'])) {
+                $this->login([
+                    'email' => $email
+                ]);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    public function login($user) {
+        // mark current user as logged-in
+        $_SESSION['user'] = [
+            'email' => $user['email']
+        ];
+
+        session_regenerate_id(true);
+    }
+
+    public function logout() {
+        $_SESSION = [];
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    }
+}
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,42 +1,22 @@
 <?php
 
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
-
-// check if user already exists
-$db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $form = new LoginForm();
-if (! $form->validate($email, $password)) {
-    return view('session/create.view.php', [
-        'errors' => $form->errors()
-    ]);
-}
+if ($form->validate($email, $password)) {
+    $auth = new Authenticator();
 
-// match user credentials
-$user = $db->query('SELECT * FROM users WHERE email = :email', [
-    'email' => $email
-])->find();
-
-if ($user) {
-    // have a user, now make sure password matches
-    if (password_verify($password, $user['password'])) {
-        login([
-            'email' => $email
-        ]);
-
-        header('location: /');
-        exit();
+    if ($auth->attempt($email, $password)) {
+        redirect('/');
     }
+
+    $form->error('email', 'No matching account found for given email address and password.');
 }
 
 return view('session/create.view.php', [
-    'errors' => [
-        'email' => 'No matching account found for given email address and password.'
-    ]
+    'errors' => $form->errors()
 ]);
